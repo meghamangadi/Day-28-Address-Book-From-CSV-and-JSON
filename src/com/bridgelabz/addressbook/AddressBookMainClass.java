957b989463662b13d23b4f0e91c542168b59b20b
megha angadi <meghamangadi@gmail.com>
2021-09-26 09:25:59 +0530

    Added: Welcome message to the user

---
+++ src/com/bridgelabz/addressbook/AddressBookMainClass.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program\n");
+
+	}
+
+}
