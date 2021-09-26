ad50b6e131b14a75ce71da67a37d3b36b854c9bb
megha angadi <meghamangadi@gmail.com>
2021-09-26 21:50:02 +0530

    Added: Program to search Person in a City or State across the multiple AddressBook

--- src/com/bridgelabz/addressbook/AddressBookMainClass.java
+++ src/com/bridgelabz/addressbook/AddressBookMainClass.java
@@ -26,7 +26,7 @@ public class AddressBookMainClass {
 
 		System.out.println("Enter your choice");
 		System.out.println(
-				"1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts 5: Display Contacts");
+				"1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts 5: Display Contacts 6: Search Person");
 		int choice = sc.nextInt();
 		switch (choice) {
 		case 1:
@@ -116,6 +116,16 @@ public class AddressBookMainClass {
 			break;
 		case 5:
 			addressbooks.displayContacts(addressBookSystem);
+			addressbooks.addContacts();
+			break;
+
+		case 6:
+			System.out.println("Search the person in perticular city or state ");
+			System.out.println("Please Enter the City Name ");
+			String cityname = sc.next();
+			System.out.println("Please Enter the State Name ");
+			String statename = sc.next();
+			addressbooks.searchPerson(cityname, statename);
 			addressbooks.addContacts();
 			break;
 		default:
@@ -260,24 +270,45 @@ public class AddressBookMainClass {
 		AddressBook Addressvalues = new AddressBook();
 		boolean isKeyPresent = addressBookSystem.containsKey(addressBookName);
 		if (isKeyPresent) {
-			AddressBook values = addressBookSystem.get(addressBookName); 
-			List<Contact> contactDetails = values.getContacts(); 
-			boolean isPresent=   contactDetails.stream().anyMatch(con->con.getFirstName().equals(contact.getFirstName())) ;
-			if(isPresent) {
+			AddressBook values = addressBookSystem.get(addressBookName);
+			List<Contact> contactDetails = values.getContacts();
+			boolean isPresent = contactDetails.stream()
+					.anyMatch(con -> con.getFirstName().equals(contact.getFirstName()));
+			if (isPresent) {
 
 				System.out.println("This peson name already persent");
-				
-			}else {
+
+			} else {
 				contactDetails.add(contact);
 				values.setContacts(contactDetails);
 				addressBookSystem.put(addressBookName, values);
 			}
-			
+
 		} else {
 			contactsLis.add(contact);
 			Addressvalues.setContacts(contactsLis);
 			addressBookSystem.put(addressBookName, Addressvalues);
 		}
+	}
+
+	public void searchPerson(String cityname, String statename) {
+		List<Contact> contactsList = new ArrayList<>();
+		for (Map.Entry<String, AddressBook> set : addressBookSystem.entrySet()) {
+			AddressBook addressBook = set.getValue();
+			contactsList = addressBook.getContacts();
+			boolean isPresent = contactsList.stream()
+					.anyMatch(con -> con.getCity().equals(cityname) || con.getState().equals(statename));
+			if (isPresent) {
+				contactsList.stream().filter(s -> s.getCity().equals(cityname) || s.getState().equals(statename))
+						.sorted().forEachOrdered(conts -> System.out.println("User name :" + conts.getFirstName()));
+
+			} else {
+
+				System.out.println("This peson not present in this city or state");
+			}
+
+		}
+
 	}
 
 }
